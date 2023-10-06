cd9b82d874c7de1eb555b3fdb317e64c64321597
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-10-06 17:25:09 -0300

    Add stat names

--- src/components/PokeStats.tsx
+++ src/components/PokeStats.tsx
@@ -23,20 +23,29 @@ const Stat = styled.li<{ value: number; name: keyof PokemonStats }>`
     name === "hp"
       ? "#FF5959"
       : name === "attack"
-      ? "#F5AC78"
-      : name === "defense"
-      ? "#FAE078"
-      : name === "special-attack"
-      ? "#9DB7F5"
-      : name === "special-defense"
-      ? "#A7DB8D"
-      : name === "speed"
-      ? "#FA92B2"
-      : "white"};
+        ? "#F5AC78"
+        : name === "defense"
+          ? "#FAE078"
+          : name === "special-attack"
+            ? "#9DB7F5"
+            : name === "special-defense"
+              ? "#A7DB8D"
+              : name === "speed"
+                ? "#FA92B2"
+                : "white"};
 
   width: calc(${(props) => props.value} * 0.08rem);
   border-radius: 0.15rem;
   padding: 0.1rem 0.4rem;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+
+	:nth-child(2) {
+		color: #0004;
+		text-shadow: 0.02rem 0.02rem 2px #00000069;
+	}
 `
 
 const statLabel: { [k in keyof PokemonStats]: string } = {
@@ -52,22 +61,28 @@ export const PokeStats: FC<PokeStatsProps> = ({ stats, className }) => {
   return (
     <StatsList className={className}>
       <Stat value={stats.hp} name="hp">
-        {statLabel["hp"]}
+        <span>{statLabel["hp"]}</span>
+        <span>{stats.hp}</span>
       </Stat>
       <Stat value={stats.attack} name="attack">
-        {statLabel["attack"]}
+        <span>{statLabel["attack"]}</span>
+        <span>{stats.attack}</span>
       </Stat>
       <Stat value={stats.defense} name="defense">
-        {statLabel["defense"]}
+        <span>{statLabel["defense"]}</span>
+        <span>{stats.defense}</span>
       </Stat>
       <Stat value={stats["attack"]} name="special-attack">
-        {statLabel["special-attack"]}
+        <span>{statLabel["special-attack"]}</span>
+        <span>{stats["attack"]}</span>
       </Stat>
       <Stat value={stats["special-defense"]} name="special-defense">
-        {statLabel["special-defense"]}
+        <span>{statLabel["special-defense"]}</span>
+        <span>{stats["special-defense"]}</span>
       </Stat>
       <Stat value={stats.speed} name="speed">
-        {statLabel["speed"]}
+        <span>{statLabel["speed"]}</span>
+        <span>{stats.speed}</span>
       </Stat>
     </StatsList>
   )
